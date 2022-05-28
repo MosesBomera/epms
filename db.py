3e9fa5c0f17eb11b065bfb6c3e9ba690ffab33a2
Moses Bomera <mosesbomera@gmail.com>
2022-05-28 23:22:15 +0300

    build: add data models

--- db.py
+++ db.py
@@ -6,15 +6,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Db Configuration.
 db = SQLAlchemy(app)
+db.init_app(app)
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
     firstname = db.Column(db.String(64))
     lastname = db.Column(db.String(64))
-    password = db.Column(db.String(64))
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -23,11 +26,11 @@ class User(db.Model):
 class Patient(db.Model):
     __tablename__ = 'patients'
     id = db.Column(db.String(36), primary_key=True)
-    name = db.Column(db.String(64))
-    email = db.Column(db.String(64), nullable=True)
-    phone = db.Column(db.String(13), nullable=True) # Account for calling code.
-    data = db.Column(db.Text())
-    comment = db.Column(db.String(100), nullable=True)
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64))
+    phone = db.Column(db.String(13)) 
+    data = db.Column(db.Text, nullable=False)
+    comment = db.Column(db.String(100))
     predictions = db.relationship('Prediction', backref='patient')
 
     def __repr__(self):
@@ -35,6 +38,7 @@ class Patient(db.Model):
 
 
 class Prediction(db.Model):
-    ___tablename___ = 'predictions'
+    __tablename__ = 'predictions'
     id = db.Column(db.Integer, primary_key=True)
-    screen_id = db.Column(db.String(36), db.ForeignKey('patients.id'))
+    patient_id = db.Column(db.String(36), db.ForeignKey('patients.id'))
+    prediction = db.Column(db.Integer, nullable=False)
