1e4350fc28268ff9f5422ce88c5b1021db68ccd1
Moses Bomera <mosesbomera@gmail.com>
2022-05-28 23:46:13 +0300

    build: add database logic to app ecosystem

--- db.py
+++ db.py
@@ -1,15 +1,6 @@
-import os
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Db Configuration.
-db = SQLAlchemy(app)
-db.init_app(app)
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
