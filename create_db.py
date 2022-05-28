51dbd5ea7bef3a5dda231803f31c43409b4e9776
Moses Bomera <mosesbomera@gmail.com>
2022-05-28 21:58:32 +0300

    feat: add User data model

--- create_db.py
+++ create_db.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+
+    def __repr__(self):
+        return f'<User {self.username}>'
