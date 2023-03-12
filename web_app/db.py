2dff864dde367c85bb4d36408b35ed9117246175
Moses Bomera <mosesbomera@gmail.com>
2023-03-12 16:25:52 +0300

    Add datetime column

--- web_app/db.py
+++ web_app/db.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql import func
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -9,6 +10,7 @@ class User(db.Model):
     password = db.Column(db.String(64), nullable=False)
     firstname = db.Column(db.String(64))
     lastname = db.Column(db.String(64))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -23,6 +25,7 @@ class Patient(db.Model):
     symptoms = db.Column(db.Text, nullable=False)
     comment = db.Column(db.String(100))
     predictions = db.relationship('Prediction', backref='patient')
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     def __repr__(self):
         return f'<Patient {self.name}>'
@@ -33,3 +36,4 @@ class Prediction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     patient_id = db.Column(db.String(36), db.ForeignKey('patients.id'))
     prediction = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
